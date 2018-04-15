df6b5bc853ac6ae1f01a300b3144bbba8c7dcb27
halejandropmartz <halejandropmartz@gmail.com>
2018-04-14 23:08:31 -0500

    Corrección de cotejamiento
    Para que caracteres del idioma español se puedan visualizar

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,14 +1,16 @@
 <?php 
-$conn = new mysqli('localhost', 'root', 'hperez123', 'spinoff');
-if ($conn->connect_errno) {
-    // La conexión falló.
-    // No se debe revelar información delicada
-	echo "Lo sentimos, este sitio web está experimentando problemas.";
-    // Algo que no se debería de hacer en un sitio público, de todas formas, es imprimir información relacionada con errores de MySQL 
-	echo "Error: Fallo al conectarse a MySQL debido a: \n";
-	echo "Errno: " . $mysqli->connect_errno . "\n";
-	echo "Error: " . $mysqli->connect_error . "\n";
-    // Podría ser conveniente mostrar algo interesante, aunque nosotros simplemente saldremos
-	exit;
-}
+	$conn = new mysqli('localhost', 'hperez', 'hperez123', 'spinoffitz');
+	if ($conn->connect_errno) {
+	    // La conexión falló.
+	    // No se debe revelar información delicada
+		echo "Lo sentimos, este sitio web está experimentando problemas.";
+	    // Algo que no se debería de hacer en un sitio público, de todas formas, es imprimir información relacionada con errores de MySQL 
+		// echo "Error: Fallo al conectarse a MySQL debido a: \n";
+		// echo "Errno: " . $mysqli->connect_errno . "\n";
+		// echo "Error: " . $mysqli->connect_error . "\n";
+	    // Podría ser conveniente mostrar algo interesante, aunque nosotros simplemente saldremos
+		exit;
+	}
+
+	mysqli_set_charset($conn,"utf8");
 ?>
